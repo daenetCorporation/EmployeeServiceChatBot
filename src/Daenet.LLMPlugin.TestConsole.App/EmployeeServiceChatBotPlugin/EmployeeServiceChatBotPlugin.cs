7069a8db92f3a41f8dfaddf061cd6177dd2816be
Anh Nguyen <anguyen@daenet.com>
2024-10-30 16:16:58 +0100

    initialize employeeservice plugin

---
+++ src/Daenet.LLMPlugin.TestConsole.App/EmployeeServiceChatBotPlugin/EmployeeServiceChatBotPlugin.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daenet.LLMPlugin.TestConsole.App.EmployeeServiceChatBotPlugin
+{
+    public class EmployeeServiceChatBotPlugin
+    {
+        private readonly EmployeeServiceChatBotPluginConfig _cfg;
+
+        public EmployeeServiceChatBotPlugin(EmployeeServiceChatBotPluginConfig cfg)
+        {
+            _cfg = cfg;
+        }
+
+
+        [KernelFunction]
+        [Description("Provides the list of names of processes")]
+        public string GetProcessInfo([Description("If set in true, it provides the detaile process information.")] bool provideDetailedInfo = false)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Process proc in Process.GetProcesses().ToList())
+            {
+                sb.AppendLine($"Proce name: {proc.ProcessName}");
+                if (provideDetailedInfo)
+                {
+                    sb.AppendLine($"Proces ID: {proc.Id}"); ;
+                    sb.AppendLine($"Proces working set: {proc.WorkingSet64}");
+                    sb.AppendLine("---------------------------------------------");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ src/Daenet.LLMPlugin.TestConsole.App/EmployeeServiceChatBotPlugin/EmployeeServiceChatBotPluginConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daenet.LLMPlugin.TestConsole.App.EmployeeServiceChatBotPlugin
+{
+    public class EmployeeServiceChatBotPluginConfig
+    {
+        public string BaseUrl { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecrete { get; set; }
+        public string TenantId { get; set; }
+        public string ApplicationUrl { get; set; }
+
+    }
+}
